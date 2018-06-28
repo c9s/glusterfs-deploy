6313ba4c17094f5d5d5296ab3de20885a75a71c0
c9s <yoanlin93@gmail.com>
2018-06-28 18:39:59 +0800

    fix heketi install and add install script

--- lib/gcloud/instance.sh
+++ lib/gcloud/instance.sh
@@ -16,7 +16,9 @@ function gcloud_instance_network_ip()
 function gcloud_instance_info()
 {
     local instance_id=$1
+    local cache_dir="deployments/$DEPLOYMENT_ID/cache"
     local cache_file="deployments/$DEPLOYMENT_ID/cache/$instance_id.json"
+    mkdir -p $cache_dir
     if [[ ! -f "$cache_file" ]]
     then
         gcloud compute instances describe --format json $instance_id > $cache_file
